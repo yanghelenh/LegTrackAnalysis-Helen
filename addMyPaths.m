ed28a28d5fe3ebd7fc58822b8bb8307144a34d67
yanghelenh <yang.helenh@gmail.com>
2021-08-31 13:28:19 -0400

    matlab version of gaussian process smoothing

--- addMyPaths.m
+++ addMyPaths.m
@@ -5,28 +5,46 @@
 % Users: change the following paths to match those on your local computer
 
 function addMyPaths() 
-    %% Animal Part Tracker code
-    APTpath = '/Users/hyang/Documents/MATLAB/APT';
+    % Determine which computer this code is running on
+    comptype = computer; % get the string describing the computer type
+    PC_STRING = 'PCWIN64';  % string for PC on 2P rig
+    MAC_STRING = 'MACI64'; %string for macbook
+    
+    % paths for windows
+    if strcmp(comptype, PC_STRING)
+        APTpath = 'C:\Users\WilsonLab\Documents\MATLAB\APT'; % APT
+        pythonPath = 'C:\ProgramData\Anaconda3\python.exe';
+        a2libPath = 'C:\Users\WilsonLab\Documents\HelenExperimentalCode\LegTrackAnalysis-Helen\a2lib';
+        analysisPath = 'C:\Users\WilsonLab\Documents\HelenExperimentalCode\LegTrackAnalysis-Helen';
+        
+    % paths for mac
+    elseif strcmp(comptype, MAC_STRING)
+        APTpath = '/Users/hyang/Documents/MATLAB/APT'; % APT
+        pythonPath = '/anaconda3/bin/python';
+        a2libPath = '/Users/hyang/Documents/LegTrackAnalysis-Helen/a2lib';
+        analysisPath = '/Users/hyang/Documents/LegTrackAnalysis-Helen';
+        
+    else
+        disp('ERROR: Paths not added. Unrecognized computer type');
+    end
+    
+    %% Add the paths
     addpath(genpath(APTpath));
+    addpath(genpath(analysisPath));
     
     %% Python functions, add to python system path
     
     [~, ~, pyLoaded] = pyversion;
     
     if ~(pyLoaded)
-        pyversion '/anaconda3/bin/python'
+        pyversion(pythonPath);
     end
     
-    a2libPath = '/Users/hyang/Documents/LegTrackAnalysis-Helen/a2lib';
     P = py.sys.path;
     if count(P,a2libPath) == 0
         insert(P,int32(0),a2libPath);
     end
     
     pyversion
-
-    %% Analysis code repository (LegTrackAnalysis-Helen)
-    analysisPath = '/Users/hyang/Documents/LegTrackAnalysis-Helen';
-    addpath(genpath(analysisPath));
 
 end
---
+++ gaussSmooth.m
@@ -0,0 +1,65 @@
+% gaussSmooth.m
+%
+% Function to smooth data by bidirectional convolution with 3.5 sigma
+%  truncated gaussian kernal.
+% Adaptation of Stephen's python function safe_interp_conv_smooth_ball_data
+%  in a2lib/proc_utils
+%
+% INPUTS:
+%   vec - vector to smooth
+%   padLen - length of padding, in samples
+%   kernLen - length of Gaussian kernal for smoothing, in samples
+%
+% OUTPUTS:
+%   smoVec - smoothed vector
+%
+% CREATED: 8/30/21 - HHY
+%
+% UPDATED:
+%   8/31/21 - HHY - fixed bug with values going outside of data range with
+%       interpolation and resampling
+%
+
+function smoVec = gaussSmooth(vec, padLen, kernLen)
+
+    % get padding
+    prePad = getInterpPad(vec, padLen, 'start');
+    postPad = getInterpPad(vec, padLen, 'end');
+    
+    % add padding to vector
+    if iscolumn(vec) % column vector
+        padVec = [prePad; vec; postPad];
+    else % row vector
+        padVec = [prePad vec postPad];
+    end
+    
+    % upsample by interpolation
+    upSampFac = 10; % factor to upsample by
+    ind = 1:length(padVec); % original sample points
+    % upsampled points
+    upSampInd = linspace(ind(1),ind(end),length(padVec) * upSampFac);
+    % upsampled vector
+    padVecUs = interp1(ind, padVec, upSampInd, 'spline');
+    
+    % gaussian kernel for kernLen samples (take upsampling into account)
+    kernLenUs = kernLen * upSampFac;
+    gaussCutoff = 3.5; % number of sigma to extend gaussian
+    % sample points for gaussian, spanning +/- 3.5 sigma
+    gaussPts = linspace(-1 * gaussCutoff, gaussCutoff, kernLenUs);
+    
+    % kernal
+    kernUs = normpdf(gaussPts);
+    kernUs = kernUs / sum(kernUs); % area under curve = 1
+    
+    % convolve in both directions and average
+    vecConvUsFwd = conv(padVecUs, kernUs, 'same');
+    vecConvUsRev = fliplr(conv(fliplr(padVecUs), fliplr(kernUs), 'same'));
+    % average
+    vecConvUs = (vecConvUsFwd + vecConvUsRev) / 2;
+    
+    % downsample, by interpolation
+    vecConv = interp1(upSampInd, vecConvUs, ind, 'spline');
+    
+    % remove padding
+    smoVec = vecConv((padLen + 1):(length(vecConv)-padLen));
+end
